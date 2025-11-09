cff897f789109999db6d0c1d7ba860bc32272889
Peter Krull <peterkrullpeter@gmail.com>
2025-11-09 13:41:03 +0100

    update ublox dependency

--- common/src/tasks/gnss_reader.rs
+++ common/src/tasks/gnss_reader.rs
@@ -1,6 +1,5 @@
-use embassy_time::{with_timeout, Duration, Instant};
-
-use crate::types::measurements::{GnssData, GnssTime};
+use embassy_time::{with_timeout, Duration};
+use ublox::{proto23::PacketRef, FixedBuffer, Parser, UbxPacket};
 
 #[embassy_executor::task]
 pub async fn main(stream_id: &'static str) -> ! {
@@ -8,19 +7,14 @@ pub async fn main(stream_id: &'static str) -> ! {
     info!("{}: Task started", ID);
 
     let mut stream = crate::serial::claim(stream_id).unwrap();
-
     let mut snd_raw_gnss_data = crate::signals::RAW_GNSS_DATA.sender();
-
-    let mut buffer = [0u8; 128];
-    let mut parse_buffer = [0u8; 128];
-    let buf = ublox::FixedLinearBuffer::new(&mut parse_buffer);
-    let mut parser = ublox::Parser::new(buf);
+    let mut parser: Parser<FixedBuffer<128>> = Parser::new_fixed();
 
     info!("{}: Entering main loop", ID);
     'parsing: loop {
         // Read serial data, but with a timeout
-        let bytes =
-            match with_timeout(Duration::from_secs(1), stream.reader.read(&mut buffer)).await {
+        let received_bytes =
+            match with_timeout(Duration::from_secs(1), stream.reader.fill_buf()).await {
                 Ok(Ok(bytes)) => bytes,
                 Ok(Err(_)) => {
                     error!("{}: Failed to read serial data", ID);
@@ -34,49 +28,22 @@ pub async fn main(stream_id: &'static str) -> ! {
 
         // Exhaustively parse bytes. This might immediately overwrite
         // a just-parsed packet, but it ensures we use the latest packet.
+        let num_received_bytes = received_bytes.len();
+        let mut ubx_parser_iter = parser.consume_ubx(received_bytes);
 
-        let sub_buffer = &buffer[..bytes];
-        let mut consumed = parser.consume(sub_buffer);
-
-        while let Some(parsed) = consumed.next() {
+        while let Some(parsed) = ubx_parser_iter.next() {
             match parsed {
                 Ok(packet) => match packet {
-                    ublox::PacketRef::NavPvt(pvt) => {
-                        let time = GnssTime {
-                            year: pvt.year(),
-                            month: pvt.month(),
-                            day: pvt.day(),
-                            hour: pvt.hour(),
-                            min: pvt.min(),
-                            sec: pvt.sec(),
-                        };
-
-                        let gnss_packet = GnssData {
-                            timestamp_us: Instant::now().as_micros(),
-                            time,
-                            fix: pvt.fix_type().into(),
-                            satellites: pvt.num_satellites(),
-                            lat_raw: pvt.lat_degrees_raw(),
-                            lon_raw: pvt.lon_degrees_raw(),
-                            altitude: pvt.height_meters() as f32,
-                            horiz_accuracy: pvt.horiz_accuracy() as f32 * 1e-3,
-                            vert_accuracy: pvt.vert_accuracy() as f32 * 1e-3,
-                            vel_north: pvt.vel_north() as f32,
-                            vel_east: pvt.vel_east() as f32,
-                            vel_down: pvt.vel_down() as f32,
-                            vel_accuracy: pvt.speed_accuracy_estimate() as f32,
-                            vel_ground: pvt.ground_speed() as f32,
-                            heading: pvt.heading_degrees() as f32,
-                            heading_accuracy: pvt.heading_accuracy_estimate() as f32,
-                            mag_declination: pvt.magnetic_declination_degrees() as f32,
-                        };
-
-                        snd_raw_gnss_data.send(gnss_packet);
+                    UbxPacket::Proto23(PacketRef::NavPvt(pvt)) => {
+                        snd_raw_gnss_data.send(pvt.into());
                     }
-                    _ => error!("{}: Unknown GNSS packet type", ID),
+                    _ => error!("[gnss_reader] Unknown UBX protocol or packet type"),
                 },
-                Err(_) => error!("{}: Error parsing GNSS packet", ID),
+                Err(_) => error!("[gnss_reader] Error parsing UBX packet"),
             }
         }
+
+        core::mem::drop(ubx_parser_iter);
+        stream.reader.consume(num_received_bytes);
     }
 }
--- common/src/types/measurements.rs
+++ common/src/types/measurements.rs
@@ -52,26 +52,40 @@ pub struct GnssTime {
 #[derive(Default, Debug, Copy, Clone, Serialize, Deserialize)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 pub struct GnssData {
+    // The device-local time of when
+    // the packet was parsed.
     pub timestamp_us: u64,
     pub time: GnssTime,
     pub fix: GnssFix,
-    pub satellites: u8,
+    pub num_satellites: u8,
 
-    pub lat_raw: i32,
-    pub lon_raw: i32,
-    pub altitude: f32,
+    /// Raw latitude coordinate, in `1e7 * deg`.
+    pub latitude_raw: i32,
+    /// Raw longitude coordinate, in `1e7 * deg`.
+    pub longitude_raw: i32,
+    /// Height above mean sea level (MSL), in `m/s`.
+    pub height_above_msl: f32,
 
-    pub horiz_accuracy: f32,
-    pub vert_accuracy: f32,
+    /// Accuracy of `velocity_north` and `velocity_east`, in `m/s`.
+    pub horizontal_accuracy: f32,
+    /// Accuracy of `velocity_down`, in `m/s`.
+    pub vertical_accuracy: f32,
 
-    pub vel_north: f32,
-    pub vel_east: f32,
-    pub vel_down: f32,
+    /// Velocity along the north-south axis, in `m/s`.
+    pub velocity_north: f32,
+    /// Velocity along the east-west axis, in `m/s`.
+    pub velocity_east: f32,
+    /// Velocity along the down-up axis, in `m/s`.
+    pub velocity_down: f32,
 
-    pub vel_accuracy: f32,
-    pub vel_ground: f32,
+    /// Speed of north-east motion, in `m/s`.
+    pub ground_speed: f32,
+    /// Accuracy of `ground_speed`, in `m/s`.
+    pub ground_speed_accuracy: f32,
 
-    pub heading: f32,
+    /// Heading of north-east motion, in radians.
+    pub heading_motion: f32,
+    /// Accuracy of `heading_motion`, in radians.
     pub heading_accuracy: f32,
     pub mag_declination: f32,
 }
@@ -82,19 +96,53 @@ pub struct GnssData {
 pub enum GnssFix {
     #[default]
     NoFix,
+    TimeOnly,
     Fix2D,
     Fix3D,
-    TimeOnly,
 }
 
 #[cfg(feature = "gnss")]
-impl From<ublox::GpsFix> for GnssFix {
-    fn from(fix: ublox::GpsFix) -> Self {
+impl From<ublox::GnssFixType> for GnssFix {
+    fn from(fix: ublox::GnssFixType) -> Self {
         match fix {
-            ublox::GpsFix::Fix2D => Self::Fix2D,
-            ublox::GpsFix::Fix3D => Self::Fix3D,
-            ublox::GpsFix::TimeOnlyFix => Self::TimeOnly,
+            ublox::GnssFixType::Fix2D => Self::Fix2D,
+            ublox::GnssFixType::Fix3D => Self::Fix3D,
+            ublox::GnssFixType::TimeOnlyFix => Self::TimeOnly,
             _ => Self::NoFix,
+        }
+    }
+}
+
+#[cfg(feature = "gnss")]
+impl<'a> From<ublox::packets::nav_pvt::proto23::NavPvtRef<'a>> for GnssData {
+    fn from(pvt: ublox::packets::nav_pvt::proto23::NavPvtRef<'a>) -> Self {
+        let time = GnssTime {
+            year: pvt.year(),
+            month: pvt.month(),
+            day: pvt.day(),
+            hour: pvt.hour(),
+            min: pvt.min(),
+            sec: pvt.sec(),
+        };
+
+        GnssData {
+            timestamp_us: embassy_time::Instant::now().as_micros(),
+            time,
+            fix: pvt.fix_type().into(),
+            num_satellites: pvt.num_satellites(),
+            latitude_raw: pvt.latitude_raw(),
+            longitude_raw: pvt.longitude_raw(),
+            height_above_msl: pvt.height_msl() as f32,
+            horizontal_accuracy: pvt.horizontal_accuracy() as f32,
+            vertical_accuracy: pvt.vertical_accuracy() as f32,
+            velocity_north: pvt.vel_north() as f32,
+            velocity_east: pvt.vel_east() as f32,
+            velocity_down: pvt.vel_down() as f32,
+            ground_speed: pvt.ground_speed_2d() as f32,
+            ground_speed_accuracy: pvt.speed_accuracy() as f32,
+            heading_motion: (pvt.heading_motion() as f32).to_radians(),
+            heading_accuracy: (pvt.heading_accuracy() as f32).to_radians(),
+            mag_declination: pvt.magnetic_declination() as f32,
         }
     }
 }
