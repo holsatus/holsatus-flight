f3c6e8d72ca36a1b2565c77b7b28734fbd6ee46e
Peter Krull <peterkrullpeter@gmail.com>
2026-07-27 17:34:57 +0200

    Use correct rate for simulated IMU (not very robust to changes)

--- device/std-test-device/src/resources.rs
+++ device/std-test-device/src/resources.rs
@@ -64,7 +64,7 @@ pub mod imu_reader {
         common::tasks::imu_reader::ImuReader::entry::<Imu<'_>>(
             imu,
             (),
-            Ticker::every(Duration::from_hz(1000)),
+            Ticker::every(Duration::from_hz(500)),
         )
         .await
     }
