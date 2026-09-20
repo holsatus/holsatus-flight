f3c3dbb7fee5a9c2112b0f80858689527debed58
Peter Krull <peterkrullpeter@gmail.com>
2026-09-20 17:04:56 +0200

    Remove load_all task which is no longer used

--- device/std-test-device/src/lib.rs
+++ device/std-test-device/src/lib.rs
@@ -86,7 +86,6 @@ fn firmware_entry(spawner: Spawner, r: Resources, sim: SimHandle) {
 
     // Might as well start the parameter storage module to get things loaded
     spawner.spawn(resources::param_storage(r.flash).unwrap());
-    spawner.spawn(common::params::load_all_task().unwrap());
 
     // ------------------ high-priority tasks -------------------
 
